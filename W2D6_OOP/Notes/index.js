238ceaf11d90403889c70a1802243117625c963c
Leo <liz1@Leo.local>
2024-12-03 22:04:13 -0800

    add new note from the meeting

--- W2D6_OOP/Notes/index.js
+++ W2D6_OOP/Notes/index.js
@@ -118,3 +118,18 @@ function Video(title){
 }
 
 const v = new Video('b');
+
+
+//
+const people = [
+    { id: 1, name: "Alice", age: 30 },
+    { id: 2, name: "Bob", age: 25 },
+    { id: 3, name: "Charlie", age: 35 },
+    { id: 4, name: "Diana", age: 28 },
+    { id: 5, name: "Ethan", age: 22 }
+];
+
+const filterPeople = people.filter((person)=> {
+    return person.id !== 3;
+});
+console.log(filterPeople);
