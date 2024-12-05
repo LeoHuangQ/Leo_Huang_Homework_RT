d83539ecd48b36733493399d5d405521fb55aafe
Leo <liz1@Leo.local>
2024-12-04 19:26:27 -0800

    upload the note of day 7

--- W2D7_Function/Note/index.js
+++ W2D7_Function/Note/index.js
@@ -15,3 +15,65 @@ const build2 = (ingredient1) => (ingredient2) => (ingredient3) =>  `${ingredient
         
 const my2 = build2('bacon22')('lettuce22')('tomato22');
 console.log(my2);
+////
+function sayHi(getPerson){
+    const person = getPerson();
+    // console.log(`hi my name is ${person.name}`);
+    return `hi my name is ${person.name}`;
+}
+
+const getPerson = () => {
+    return {
+        name: 'jack'
+    }
+}
+
+console.log(sayHi(getPerson));
+
+///
+
+function stockController(){
+    let stock = 0;
+    return {
+        buyStock: function (count){
+            stock += count;
+        },
+        show: function (){
+            return stock;
+        },
+        sellStock: function(count = 0){
+            if (count > stock) {
+                stock = 0;
+                return;
+            }
+            stock -= count;
+            // return stock;
+        }
+    }
+}
+
+const stock = stockController();
+stock.buyStock(30);
+console.log('After buying stock 30, stock is ', stock.show());
+// typeof NaN (not a number) is number
+// stock.buyStock();
+// console.log(stock.show() , typeof stock.show());
+stock.sellStock(10);
+console.log('After selling stock 10, stock is ', stock.show());
+
+stock.sellStock(100);
+console.log('After selling stock 100, stock is ', stock.show());
+
+
+// scope: variable search will find the nearest one
+let a = 'global';
+(function bar(){
+    const a = 'bar';
+    (function foo(){
+        // const a = 'foo';
+        console.log(a);
+    }
+    )();
+})();
+
+
