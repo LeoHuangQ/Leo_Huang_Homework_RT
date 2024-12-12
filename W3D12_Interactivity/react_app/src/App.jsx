3b15bf212a38299aa281661e32a8014ae7cf2176
Leo <liz1@Leo.local>
2024-12-12 00:31:05 -0800

    add the react app practise

---
+++ W3D12_Interactivity/react_app/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Build from './MyList'
+
+
+
+function App() {
+  
+  return (
+    <>
+      <div> 
+        <Build/>
+      </div>
+    </>
+  )
+}
+
+export default App
+
